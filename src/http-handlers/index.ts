beda60791d470d25d4114eebcab31f68d40dd3d4
Victor Kozhevnikov <victor_kozhevnikov@epam.com>
2017-11-02 12:25:18 +0300

    refactor handler file names

--- src/http-handlers/index.ts
+++ src/http-handlers/index.ts
@@ -1,4 +1,4 @@
 export * from './handler';
-export * from './plain-text-handler';
-export * from './html-handler';
-export * from './json-handler';
+export * from './plain-text.handler';
+export * from './html.handler';
+export * from './json.handler';
