6f32bd0fc507f4e89962b929a398eb31f96ebba8
Victor Kozhevnikov <victor_kozhevnikov@epam.com>
2017-11-10 12:04:45 +0300

    task 5.2 and 5.3 using jsonwebtoken

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,32 @@
+import * as jwt from 'jsonwebtoken';
+
 import { Credentials } from './credentials';
 import { TokenResponse } from './token-response';
 
+const secret = 'secret';
 const userName = 'user';
 const allowedEmail = 'user@example.com';
 const allowedPassword = 'password';
-const allowedToken = '00e52225e0634e8aa10f7c45dbf6ea6f';
+// const allowedToken = '00e52225e0634e8aa10f7c45dbf6ea6f';
 
 export class AuthService {
   public issueToken(credentials: Credentials): TokenResponse | null {
     if (credentials.email === allowedEmail && credentials.password === allowedPassword) {
-      return {
-        user: { email: allowedEmail, username: userName },
-        token: allowedToken
-      };
+      const user = { email: allowedEmail, username: userName };
+      const token = jwt.sign(user, secret);
+      return { user, token };
     } else {
       return null;
     }
   }
 
   public validateToken(token: string): boolean {
-    return token === allowedToken;
+    try {
+      jwt.verify(token, secret);
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
 
